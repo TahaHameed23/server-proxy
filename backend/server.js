f4332f2edfa18c4e2df6d657acdfb0ab26ba845c
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2023-10-24 21:33:00 +0530

    fix:route

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from 'express';
 // import dotenv from 'dotenv';
 const app = express();
 const PORT = 3000;
-// app.get('/', (req, res) => {
-//   res.send({ message: 'hello' });
-// });
+app.get('/', (req, res) => {
+  res.send({ message: 'hello' });
+});
 
 app.get('/api/jokes', (req,res) => {
   const jokes = [
