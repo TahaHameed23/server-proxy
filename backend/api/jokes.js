4f3322593fb981740dcbfca22ee2fa6ca713ba8f
TahaHameed23 <89855847+TahaHameed23@users.noreply.github.com>
2023-11-04 23:08:13 +0530

    add:C:/Program Files/Git/api dir

---
+++ backend/api/jokes.js
@@ -0,0 +1,27 @@
+export const jokes = [
+    {
+      id: 1,
+      title: 'A joke',
+      content: 'hahaha',
+    },
+    {
+      id: 2,
+      title: 'A joke',
+      content: 'hahaha',
+    },
+    {
+      id: 3,
+      title: 'A joke',
+      content: 'hahaha',
+    },
+    {
+      id: 4,
+      title: '4th joke',
+      content: 'hahaha',
+    },
+    {
+      id: 1,
+      title: 'Fifth joke',
+      content: 'LOL',
+    },
+  ];
--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,11 @@
 import express from 'express';
+import { jokes } from './api/jokes.js';
 // import dotenv from 'dotenv';
 const app = express();
 const PORT = 4000;
 
 
 app.get('/api/jokes', (req,res) => {
-  const jokes = [
-    {
-      id: 1,
-      title: 'A joke',
-      content: 'hahaha',
-    },
-    {
-      id: 2,
-      title: 'A joke',
-      content: 'hahaha',
-    },
-    {
-      id: 3,
-      title: 'A joke',
-      content: 'hahaha',
-    },
-    {
-      id: 4,
-      title: '4th joke',
-      content: 'hahaha',
-    },
-    {
-      id: 1,
-      title: 'Fifth joke',
-      content: 'LOL',
-    },
-  ];
   res.send(jokes);
 });
 
